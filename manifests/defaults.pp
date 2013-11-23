d0080e5ce9a2b3e028535d53134f9854118c87d3
narkisr <narkisr@gmail.com>
2013-11-23 02:23:38 +0200

    adding defaults

---
+++ manifests/defaults.pp
@@ -0,0 +1,44 @@
+# default smokeping settings to start with
+class smokeping::defaults {
+
+  $probes = [
+    {name => 'FPing', binary  => '/usr/bin/fping'},
+    {name => 'DNS', binary => '/usr/bin/dig'},
+  ]
+
+  package{'sendmail':
+    ensure  => present
+  }
+
+  class {'smokeping':
+    mode   => 'standalone',
+    probes => $probes,
+  }
+
+  smokeping::target { 'World':
+    menu      => 'World',
+    pagetitle => 'Connection to the World',
+  }
+
+  smokeping::target { 'Google':
+    hierarchy_parent => 'World',
+    hierarchy_level  => 2,
+    menu             => 'google.com',
+    pagetitle        => 'Google',
+  }
+
+  smokeping::target { 'Google-fping':
+    hierarchy_parent => 'Google',
+    hierarchy_level  => 3,
+    menu             => 'google.com ping',
+    host             => 'google.com',
+  }
+
+  smokeping::target { 'Google-DNS':
+    hierarchy_parent => 'Google',
+    hierarchy_level  => 3,
+    probe            => 'DNS',
+    menu             => 'google.com dns',
+    host             => '8.8.8.8',
+  }
+}
