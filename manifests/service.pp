3618d3f14fa6d1fc73f1e98af075f1874b200dc0
ronen <narkisr@gmail.com>
2016-06-17 03:12:07 +0300

    stopping apache and starting lighttpd

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,27 @@
+# smokeping service setup
 class smokeping::service {
 
-  $ensure = $smokeping::start ? { true => running, false => stopped, default => undef }
+  $ensure = $smokeping::start ? {
+    true => running, false => stopped, default => undef
+  }
 
-  if($virtual!='docker'){
+  if $::virtual != 'docker' {
     service { 'smokeping':
-      ensure   => $ensure,
-      enable   => $smokeping::enable,
+      ensure => $ensure,
+      enable => $smokeping::enable,
     }
+
+    service { 'apache2':
+      ensure => stopped,
+      enable => false
+    } ~>
+
+    service { 'lighttpd':
+      ensure => running,
+      enable => true
+    }
+
+
   }
 
 }
